0d0fbf4fc8384281e22a39bfab86f665179d5ced
ajmaaaa <ajmaaa.dev@gmail.com>
2026-06-02 22:15:17 +0700

    feat: menambahkan test_geometry.py

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,15 +1,26 @@
 import unittest
 from src.core.geometry import catmull_rom
 
+
 class TestGeometry(unittest.TestCase):
+
     def test_catmull_rom(self):
+        # Empat titik kontrol untuk membentuk kurva Catmull-Rom
         p0 = (0, 0)
         p1 = (1, 1)
         p2 = (2, 0)
         p3 = (3, -1)
-        # Evaluasi spline di tengah p1 dan p2 (t=0.5)
+
+        # Menghitung titik kurva pada parameter t = 0.5
         res = catmull_rom(p0, p1, p2, p3, 0.5)
-        
+
+        # Memastikan hasil perhitungan valid
         self.assertIsNotNone(res)
+
+        # Hasil harus berupa koordinat (x, y)
         self.assertIsInstance(res, tuple)
         self.assertEqual(len(res), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
